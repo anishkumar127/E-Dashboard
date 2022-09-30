528c84741708634ef88560dbb2cd4186d0f7676c
Anish Kumar <anishbishnoi127@gmail.com>
2022-10-01 00:06:59 +0530

    code added

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
-const app = express();
-app.get("/",(req,res) => {
-res.send("app is working...");
-});
+const mongoose = require("mongoose");
 
+const app = express();
+// app.get("/",(req,res) => {
+// res.send("app is working...");
+// });
+
+const connectDB = () =>{
+    mongoose.connect("mongodb://localhost:27017//e-com");
+    const productSchema = new mongoose.Schema({});
+    const product = mongoose.model("products",productSchema);
+}
 app.listen(8080);
